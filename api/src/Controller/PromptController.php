018e7e7b3b4741fcb6592f05a8885563d7835b1a
Timothy Yoder <tyoder@atla.com>
2018-05-15 17:40:12 -0500

    Changed to 5 lines

--- api/src/Controller/PromptController.php
+++ api/src/Controller/PromptController.php
@@ -40,7 +40,7 @@ class PromptController extends AbstractController {
     foreach ($story_line_objects as $story_line_object) {
       $prompt_lines[] = $story_line_object->getLine();
     }
-    $prompt_lines = array_slice($prompt_lines, -3, 3);
+    $prompt_lines = array_slice($prompt_lines, -5, 5);
 
     $elements = [];
     foreach ($elements_objects as $elements_object) {
